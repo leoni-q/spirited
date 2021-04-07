84d79498a385a9819a483bce84e4105af2250b93
Leon <magisterleoni@gmail.com>
2021-04-07 19:50:30 +0200

    fix contract logic

--- core/contracts/Spirited.sol
+++ core/contracts/Spirited.sol
@@ -10,35 +10,35 @@ contract Spirited is ERC721, Ownable {
 
     AggregatorV3Interface private priceFeed;
 
-    uint thousandDollarsZerosCount = 10 ** 11;
-
     bytes32[] names;
 
     mapping(uint => bytes32[12]) idToTokenURIs;
     mapping(uint => string) tokenIdToName;
-    mapping(uint => uint) btcPriceToTokenURIOrdinal;
 
     constructor(address _priceFeedAddress, bytes32[] memory _initialTokenURIs) public ERC721("Spirited", "SPR") {
         priceFeed = AggregatorV3Interface(_priceFeedAddress);
-        uint id = 0;
-        for (uint i; i < _initializeTokenURIs.length; i++) {
-            idToTokenURIs[id].push(_initializeTokenURIs[i]);
-            if (i == 11) {
+        uint id;
+        uint tokenURIOrdinal;
+        for (uint i; i < _initialTokenURIs.length; i++) {
+            idToTokenURIs[id][tokenURIOrdinal] = _initialTokenURIs[i];
+            tokenURIOrdinal++;
+            if (i % 11 == 0) {
                 id++;
+                tokenURIOrdinal = 0;
             }
         }
     }
 
-    function mintToken(bytes32 memory _name) external onlyOwner {
+    function mintToken(bytes32 _name) external onlyOwner {
         uint256 newId = names.length;
         names.push(_name);
         _safeMint(_msgSender(), newId);
     }
 
     function changeTokenURIBasedOnBtcPrice(uint _tokenId) external onlyOwner {
-        uint price;
-        (,price,,,,) = priceFeed.latestRoundData();
-        _setTokenUriByBtcPrice(price, _tokenId);
+        int price;
+        (, price,,,) = priceFeed.latestRoundData();
+        _setTokenUriByBtcPrice(uint(price), _tokenId);
     }
 
     function getTokenURI(uint256 tokenId) public view returns (string memory) {
@@ -46,34 +46,25 @@ contract Spirited is ERC721, Ownable {
     }
 
     function _setTokenUriByBtcPrice(uint _btcPrice, uint _tokenId) private {
-        if (_btcPrice < 50000 * thousandDollarsZerosCount) {
-            _setTokenURI(_tokenId, tokenURIs[0]);
-        } else if (_isPriceWithin(_btcPrice, 50, 55)) {
-            _setTokenURI(_tokenId, tokenURIs[1]);
-        } else if (_isPriceWithin(_btcPrice, 55, 60) {
-            _setTokenURI(_tokenId, tokenURIs[2]);
-        } else if (_isPriceWithin(_btcPrice, 60, 65) {
-            _setTokenURI(_tokenId, tokenURIs[3]);
-        } else if (_isPriceWithin(_btcPrice, 65, 70) {
-            _setTokenURI(_tokenId, tokenURIs[4]);
-        } else if (_isPriceWithin(_btcPrice, 70, 75) {
-            _setTokenURI(_tokenId, tokenURIs[5]);
-        } else if (_isPriceWithin(_btcPrice, 75, 80) {
-            _setTokenURI(_tokenId, tokenURIs[6]);
-        } else if (_isPriceWithin(_btcPrice, 80, 85) {
-            _setTokenURI(_tokenId, tokenURIs[7]);
-        } else if (_isPriceWithin(_btcPrice, 85, 90) {
-            _setTokenURI(_tokenId, tokenURIs[8]);
-        } else if (_isPriceWithin(_btcPrice, 90, 95) {
-            _setTokenURI(_tokenId, tokenURIs[9]);
-        } else if (_isPriceWithin(_btcPrice, 95, 100) {
-            _setTokenURI(_tokenId, tokenURIs[10]);
-        } else if (_btcPrice > 100 * thousandDollarsZerosCount) {
-            _setTokenURI(_tokenId, tokenURIs[11]);
-        } else {}
+        uint tokenURIOrdinal = _getTokenURIOrdinal(_btcPrice);
+        _setTokenURI(_tokenId, string(abi.encodePacked(idToTokenURIs[_tokenId][tokenURIOrdinal])));
     }
 
-    function _isPriceWithin(uint _price, uint _from, uint _to) pure returns (bool) {
-        return _price >= _from * thousandDollarsZerosCount && price < _to * thousandDollarsZerosCount;
+    function _getTokenURIOrdinal(uint _btcPrice) private pure returns (uint) {
+        uint ordinal;
+        if (_btcPrice < 50 * 10 ** 11) {
+            return 0;
+        } else if (_btcPrice >= 100 * 10 ** 11) {
+            return 11;
+        } else {
+            uint firstTwoDigitsOfBtcPrice = _btcPrice / 10 ** 11;
+            uint roundedAndMultiplied = _roundUpToDivisionOfFive(firstTwoDigitsOfBtcPrice) * 2;
+            return roundedAndMultiplied / 10 % 10;
+        }
+    }
+
+    function _roundUpToDivisionOfFive(uint _n) private pure returns (uint) {
+        return (_n + 4) / 5 * 5;
     }
 }
+
