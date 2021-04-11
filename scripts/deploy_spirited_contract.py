f492f22cdb4e5efe853181387273f8dc20793cb5
Leon <magisterleoni@gmail.com>
2021-04-11 19:35:16 +0200

    refactor

--- scripts/deploy_spirited_contract.py
+++ scripts/deploy_spirited_contract.py
@@ -7,7 +7,8 @@ from brownie import Spirited, MockV3Aggregator, network, accounts, config
 def main():
     if network.show_active() in ['kovan', 'rinkeby', 'mainnet']:
         dev = accounts.add(os.getenv(config['wallets']['from_key']))
-        return Spirited.deploy(config['networks'][network.show_active()]['btc_usd_price_feed'], {'from': dev})
+        return Spirited.deploy(config['networks'][network.show_active()]['btc_usd_price_feed'], {'from': dev},
+                               publish_source=True)
     else:
         mock_price_feed = MockV3Aggregator.deploy(18, 50_000_000_000_00, {'from': accounts[0]})
         return Spirited.deploy(mock_price_feed.address, {'from': accounts[0]})
